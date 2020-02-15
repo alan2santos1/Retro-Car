59c8fa4d08d4d710caa54c81492558e8dcf62700
Alan Santos Molina <alansantos@MacBook-Pro-de-Alan-2.local>
2020-02-14 18:42:32 -0600

    nsafjkhngiubfdgliusabe<

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const PORT = process.env.PORT || 7770;
 const URL_MONGO = "mongodb+srv://fer:cintaroja123@cluster0-2cnn6.mongodb.net/test?retryWrites=true&w=majority";
 
 const carros = require('./controllers/Carros.js');
+app.use(cors());
 
 app.use(express.json());
 app.use('/Carros', carros);
-app.use(cors());
 
 mongoose.connect(URL_MONGO,{ useNewUrlParser: true, useUnifiedTopology: true }, (error) => {
     if(!error){
